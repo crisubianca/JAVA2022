cbf31336693df70c289aca4417b9c314ee607355
Bianca Crișu <100210311+crisubianca@users.noreply.github.com>
2022-03-03 19:29:39 +0200

    Create Problem.java

---
+++ Laborator2/Laborator2/src/lab2/Problem.java
@@ -0,0 +1,22 @@
+/*
+ * FII JAVA 2022
+ */
+package lab2;
+
+/**
+ *
+ * @author Adm
+ */
+public class Problem {
+
+    private Event[] events;
+
+    public Event[] getEvents() {
+        return events;
+    }
+
+    public void setEvents(Event... events) {
+        this.events = events;
+    }
+
+}
