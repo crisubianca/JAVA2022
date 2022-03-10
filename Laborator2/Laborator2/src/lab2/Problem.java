ae0376faa370074f57e4161021523bb760769d32
Bianca Crișu <100210311+crisubianca@users.noreply.github.com>
2022-03-10 18:18:09 +0200

    Update Problem.java

--- Laborator2/Laborator2/src/lab2/Problem.java
+++ Laborator2/Laborator2/src/lab2/Problem.java
@@ -9,14 +9,44 @@ package lab2;
  */
 public class Problem {
 
-    private Event[] events;
+    private Event[] events = new Event[6];
+    private Room[] rooms = new Room[5];
+    private int nrOfRooms = 0;
+    private int nrOfEvents = 0;
 
+    public Problem() {
+    }
+
+    
+
+    public Problem(Event[] events) {
+        this.events = events;
+    }
+
+    public Problem(Room[] rooms) {
+        this.rooms = rooms;
+    }
+    
     public Event[] getEvents() {
         return events;
     }
 
-    public void setEvents(Event... events) {
-        this.events = events;
+    public void addEvent(Event event) {
+        events[nrOfEvents ++] = event;
     }
 
+    public Room[] getRooms() {
+        return rooms;
+    }
+
+    public void addRoom (Room room) {
+        rooms[nrOfRooms ++] = room;
+    }
+
+    @Override
+    public String toString() {
+        return "Problem{" + "events=" + events + ", rooms=" + rooms + '}';
+    }
+    
+    
 }
