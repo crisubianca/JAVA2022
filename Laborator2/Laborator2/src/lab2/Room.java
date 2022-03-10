5bfb775529e5030275276ef8dc8012892aa45c8a
Bianca Crișu <100210311+crisubianca@users.noreply.github.com>
2022-03-10 18:18:06 +0200

    Update Room.java

--- Laborator2/Laborator2/src/lab2/Room.java
+++ Laborator2/Laborator2/src/lab2/Room.java
@@ -3,6 +3,8 @@
  */
 package lab2;
 
+import java.util.Objects;
+
 /**
  *
  * @author Adm
@@ -54,5 +56,29 @@ public class Room {
     public String toString() {
         return "Room{" + "name=" + name + ", capacity=" + capacity + ", type=" + type + '}';
     }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 79 * hash + Objects.hashCode(this.name);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Room other = (Room) obj;
+        return Objects.equals(this.name, other.name);
+    }
+    
+    
 
 }
