dc2357ab494f904570b0b270a5cf308584cf2911
Bianca Crișu <100210311+crisubianca@users.noreply.github.com>
2022-03-17 17:52:07 +0200

    Update Storage.java

--- Laborator3/Laborator3/src/lab3/Storage.java
+++ Laborator3/Laborator3/src/lab3/Storage.java
@@ -7,4 +7,25 @@ package lab3;
  */
 public interface Storage {
     int getStorageCapacity();
+
+    default int changeUnitOfStorageCapacity(String requestedUnit, int storageCapacity){
+       String mb = "Megabyte";
+       String kb = "Kilobyte";
+       String b = "Byte";
+       int returnStorageCapacity = 0;
+       
+       if(requestedUnit.equals(mb) ==  true) {
+           returnStorageCapacity = storageCapacity * 1000;
+       }
+       else
+           if(requestedUnit.equals(kb) == true){
+               returnStorageCapacity = storageCapacity * 1000000;
+           }
+       else
+               if(requestedUnit.equals(b) == true){
+                   returnStorageCapacity = storageCapacity * 1000000000;
+               }
+       return returnStorageCapacity;
+  
+    }
 }
