9e31c389e4e6a63cb87ba98928b1a97fe54f9c40
Bianca Crișu <100210311+crisubianca@users.noreply.github.com>
2022-03-17 17:51:48 +0200

    Update Laborator3.java

--- Laborator3/Laborator3/src/lab3/Laborator3.java
+++ Laborator3/Laborator3/src/lab3/Laborator3.java
@@ -10,25 +10,58 @@ public class Laborator3 {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        Computer c1 = new Computer("100", 16, "A", "v1", "300");
-        Computer c2 = new Computer("102", 8, "B", "v6", "302");
+    public static void main(String[] args) 
+    {
+        Computer v1 = new Computer("100", 16, "Computer A", "v1", "300");
+        Computer v6 = new Computer("102", 8, "Computer B", "v6", "302");
         
-        Router r1 = new Router("100", "A", "v2", "300");
-        Router r2 = new Router("102", "B", "v5", "302");
+        v1.setUnitOfStorageCapacity("GB");
+        v6.setUnitOfStorageCapacity("GB");
+        v1.setStorageCapacity(v1.changeUnitOfStorageCapacity("Megabyte", 16));
+        v1.setUnitOfStorageCapacity("MB");
         
-        Switch s1 = new Switch("A", "v3", "300");
-        Switch s2 = new Switch("B", "v4", "302");
+        Router v2 = new Router("100", "Router A", "v2", "300");
+        Router v5 = new Router("102", "Router B", "v5", "302");
         
-       Network n1 = new Network();
-       n1.addNode(c1);
-       n1.addNode(c2);
-       n1.addNode(r1);
-       n1.addNode(r2);
-       n1.addNode(s1);
-       n1.addNode(s2);
-       
+        Switch v3 = new Switch("Switch A", "v3", "300");
+        Switch v4 = new Switch("Switch B", "v4", "302");
+        
+        v1.setCost(v2, 10);
+        v1.setCost(v3, 50);
+        v2.setCost(v3, 20);
+        v2.setCost(v4, 20);
+        v2.setCost(v5, 20);
+        v3.setCost(v4, 10);
+        v4.setCost(v5, 30);
+        v4.setCost(v6, 10);
+        v5.setCost(v6, 20);
+        
+        Network n1 = new Network();
+        n1.addNode(v1);
+        n1.addNode(v2);
+        n1.addNode(v3);
+        n1.addNode(v4);
+        n1.addNode(v5);
+        n1.addNode(v6);
+        
         System.out.println(n1);
+        
+        System.out.println("The complete network:");
+        System.out.println("From: " + v1.name);
+        v1.getCost();
+        
+        System.out.println("From: " + v2.name);
+        v2.getCost();
+        
+        System.out.println("From: " + v3.name);
+        v3.getCost();
+        
+        System.out.println("From: " + v4.name);
+        v4.getCost();
+        
+        System.out.println("From: " + v5.name);
+        v5.getCost();
+
     }
     
 }
