46683101ed9f6c165456771aca78a69cf0be5f29
Bianca Crișu <100210311+crisubianca@users.noreply.github.com>
2022-03-17 17:52:01 +0200

    Update Node.java

--- Laborator3/Laborator3/src/lab3/Node.java
+++ Laborator3/Laborator3/src/lab3/Node.java
@@ -1,6 +1,9 @@
 
 package lab3;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  *
  * @author Adm
@@ -9,13 +12,23 @@ public abstract class Node implements Comparable <Node> {
     protected String name;
     protected String location;
     protected String hardwareAddress;
+    
+    private Map<Node, Integer> cost = new HashMap<>();
 
     public Node(String name, String location, String hardwareAddress) {
         this.name = name;
         this.location = location;
         this.hardwareAddress = hardwareAddress;
     }
-    
+
+    public void getCost() {
+        for(Map.Entry<Node, Integer> entry : cost.entrySet())
+            System.out.println("To: " + entry.getKey() + ", Cost: " + entry.getValue());
+    }
+
+    public void setCost(Map<Node, Integer> cost) {
+        this.cost = cost;
+    }
     
 
     public String getName() {
@@ -43,10 +56,14 @@ public abstract class Node implements Comparable <Node> {
     }
 
     @Override
-    public int compareTo(Node o) {
-        if(o.getName() == null)
+    public int compareTo(Node node) {
+        if(node.getName() == null)
             throw new NullPointerException();
-        return this.name.compareTo(o.getName());
+        return this.name.compareTo(node.getName());
     }
+    
+    public void setCost(Node node, int value) {
+        cost.put(node, value);
+    } 
     
 }
