e6356ce15254046a345e295f662823d52e9705f5
Bianca Crișu <100210311+crisubianca@users.noreply.github.com>
2022-03-17 19:36:34 +0200

    Create Street.java

---
+++ Laborator4/Laborator4/src/main/java/lab4/laborator4/Street.java
@@ -0,0 +1,33 @@
+package lab4.laborator4;
+
+/**
+ *
+ * @author Adm
+ */
+public class Street {
+    private String name;
+    private int length;
+
+    public Street(String name, int length) {
+        this.name = name;
+        this.length = length;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+    
+    
+}
