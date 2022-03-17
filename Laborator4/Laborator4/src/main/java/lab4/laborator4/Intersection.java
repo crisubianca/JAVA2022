615b52cb5c70b07afc5b6992ec119f036efc2437
Bianca Crișu <100210311+crisubianca@users.noreply.github.com>
2022-03-17 19:36:30 +0200

    Create Intersection.java

---
+++ Laborator4/Laborator4/src/main/java/lab4/laborator4/Intersection.java
@@ -0,0 +1,23 @@
+package lab4.laborator4;
+
+/**
+ *
+ * @author Adm
+ */
+public class Intersection {
+    private String name;
+
+    public Intersection(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    
+}
