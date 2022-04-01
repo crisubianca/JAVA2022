b7e2bdd9ac22099686bc261c6581773fd92f5f42
Bianca Crișu <100210311+crisubianca@users.noreply.github.com>
2022-04-01 12:38:49 +0300

    Create Item.java

---
+++ Laborator5/Main/src/main/java/lab5/main/Item.java
@@ -0,0 +1,41 @@
+package lab5.main;
+import java.io.*;
+import java.nio.file.*;
+
+public abstract class Item implements Serializable {
+    private String id;
+    private String title;
+    private String location; //file name or Web page
+
+    public Item(String id, String title, String location) {
+        this.id = id;
+        this.title = title;
+        this.location = location;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+    
+
+}
