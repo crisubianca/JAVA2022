71993b7bedbd7d77765a1ce8b6a56144840e4921
Bianca Crișu <100210311+crisubianca@users.noreply.github.com>
2022-04-01 12:38:54 +0300

    Create CustomException.java

---
+++ Laborator5/Main/src/main/java/lab5/main/CustomException.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lab5.main;
+package Exceptions;
+
+public class CustomException extends Exception {
+
+    public CustomException(String msg) {
+        super(msg);
+    }
+}
