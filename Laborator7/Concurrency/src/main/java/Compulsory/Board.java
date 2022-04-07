af5af6f89e9840b519702543a63ceacabc2c4f4c
Bianca Crișu <100210311+crisubianca@users.noreply.github.com>
2022-04-08 01:36:13 +0300

    Update Board.java

--- Laborator7/Concurrency/src/main/java/Compulsory/Board.java
+++ Laborator7/Concurrency/src/main/java/Compulsory/Board.java
@@ -3,6 +3,28 @@ package Compulsory;
  *
  * @author Adm
  */
+import java.util.ArrayList;
+import java.util.List;
+
 public class Board {
+    //a collection for words
+    private final List<String> wordList;
+
+    Board() {
+        wordList = new ArrayList<>();
+    }
+
+    synchronized final void addWord ( Player p , String word ) throws NullPointerException {
+        if ( word == null ) {
+            throw new NullPointerException("Please enter a word!");
+        }
+        if( p == null ) {
+            throw new NullPointerException("An existing player must introduce a word!");
+        }
+        wordList.add(word);
+        System.out.println( p.getName() + " submitted the word " + word );
+    }
+    
+    
     
 }
