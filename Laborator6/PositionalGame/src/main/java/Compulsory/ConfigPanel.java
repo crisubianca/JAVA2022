28f291af933aaf0a7701e642c90461a385eedb00
Bianca Crișu <100210311+crisubianca@users.noreply.github.com>
2022-04-07 19:11:15 +0300

    Update ConfigPanel.java

--- Laborator6/PositionalGame/src/main/java/Compulsory/ConfigPanel.java
+++ Laborator6/PositionalGame/src/main/java/Compulsory/ConfigPanel.java
@@ -33,21 +33,19 @@ public class ConfigPanel extends JPanel {
         add(createBtn, BorderLayout.CENTER);
     }
 
-    public int getRows() {
-        return rows;
-    }
-
     public void setRows(int rows) {
         this.rows = rows;
-    }
-
-    public int getCols() {
-        return cols;
     }
 
     public void setCols(int cols) {
         this.cols = cols;
     }
-    
+        public int getRows() {
+        return (Integer) spinner1.getValue();
+    }
+
+    public int getCols() {
+        return (Integer) spinner2.getValue();
+    }
     
 }
