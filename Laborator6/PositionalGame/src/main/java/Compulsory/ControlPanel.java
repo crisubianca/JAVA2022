ebb921efa844a2eff01fda450f1728f25f0a62ff
Bianca Crișu <100210311+crisubianca@users.noreply.github.com>
2022-04-07 18:52:05 +0300

    Update ControlPanel.java

--- Laborator6/PositionalGame/src/main/java/Compulsory/ControlPanel.java
+++ Laborator6/PositionalGame/src/main/java/Compulsory/ControlPanel.java
@@ -6,6 +6,10 @@ package Compulsory;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
+import java.io.File;
+import java.io.IOException;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import javax.imageio.ImageIO;
 public class ControlPanel extends JPanel {
     final MainFrame frame;
     JButton saveBtn = new JButton("Save");
@@ -31,12 +35,55 @@ public class ControlPanel extends JPanel {
         exitBtn.addActionListener(this::exitGame);
     }
     
-    private void saveGame(ActionEvent e){
-        
+    private void saveGame(ActionEvent e) {
+        JFileChooser fileChooser = new JFileChooser();
+
+        // Only choose files of this type
+        fileChooser.setFileFilter(new FileNameExtensionFilter("*.png", "png"));
+
+        // If the user selects to save the file
+        int option = fileChooser.showSaveDialog(null);
+        if (option == JFileChooser.APPROVE_OPTION) {
+            File fileToSave = fileChooser.getSelectedFile();
+            try {
+                ImageIO.write(frame.canvas.image, "PNG", new File(fileToSave.getAbsolutePath()));
+                System.out.println("Saved an image.");
+            }
+            catch (IOException exc) {
+                System.out.println(exc.getMessage());
+            }
+        }
+        else if (option == JFileChooser.CANCEL_OPTION) {
+            System.out.println("Canceled the saving operation.");
+        }
+        else {
+            System.out.println("An error occurred during saving.");
+        }
     }
-    
-    private void loadGame(ActionEvent e){
-    
+
+    private void loadGame(ActionEvent e) {
+        JFileChooser fileChooser = new JFileChooser();
+        fileChooser.setFileFilter(new FileNameExtensionFilter("*.png", "png"));
+
+        // The user selected to load an image
+        int option = fileChooser.showOpenDialog(null);
+        if (option == JFileChooser.APPROVE_OPTION) {
+            try {
+                Image image = ImageIO.read(new File(fileChooser.getSelectedFile().getAbsolutePath()));
+                frame.getCanvas().offscreen.drawImage(image, 0, 0, frame.getCanvas());
+                frame.getCanvas().repaint();
+                System.out.println("Loaded an image.");
+            }
+            catch (IOException exc) {
+                System.out.println(exc.getMessage());
+            }
+        }
+        else if (option == JFileChooser.CANCEL_OPTION) {
+            System.out.println("Canceled the loading operation.");
+        }
+        else {
+            System.out.println("An error occurred during loading.");
+        }
     }
     
     private void exitGame(ActionEvent e){
