f8993b9d00254e48890c2c51e29548bdaf20c990
Bianca Crișu <100210311+crisubianca@users.noreply.github.com>
2022-04-08 01:36:24 +0300

    Update Player.java

--- Laborator7/Concurrency/src/main/java/Compulsory/Player.java
+++ Laborator7/Concurrency/src/main/java/Compulsory/Player.java
@@ -3,23 +3,66 @@ package Compulsory;
  *
  * @author Adm
  */
-public class Player implements Runnable{
+import java.util.List;
+
+public class Player implements Runnable {
     private String name;
     private Game game;
-    private boolean running;
-    
-    public Player(String name) { this.name = name; }
-    
-    private boolean submitWord() {
-        List<Tile> extracted = game.getBag().extractTiles(7);
-        if(extracted.isEmpty()){
-           return false;
+
+    Player(String name) {
+        this.name = name;
+    }
+
+    private boolean createSubmitWord() throws InterruptedException {
+        List extracted = game.getBag().extractLetters(1);
+        if (extracted.isEmpty()) {
+            return false;
         }
-        //create a word with all the extracted tiles
-        game.getBoard().addWord(this, word);
-        //make the player sleep 50ms
+        StringBuilder word = new StringBuilder();
+        for (int i = 0; i < 10; i++) {
+            word.append(extracted.get(0));
+        }
+//       if( !game.getDictionary().getWords().contains(word.toString()) )
+//         return false;
+        game.getBoard().addWord(this, word.toString());
+        Thread.sleep(300);
         return true;
     }
-    
-    //implement the run method
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName( String name ) {
+        this.name = name;
+    }
+
+    public Game getGame() {
+        return game;
+    }
+
+    public void setGame( Game game ) {
+        this.game = game;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            try {
+                if (!createSubmitWord()) {
+                    break;
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    //implement the run() method, that will repeatedly create and submit words
+    //implement the toString() method
 }
