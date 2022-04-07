a0112e54f3ab3a7718b14ee3e208b7c0a0a04921
Bianca Crișu <100210311+crisubianca@users.noreply.github.com>
2022-04-07 19:29:56 +0300

    Create Game.java

---
+++ Laborator7/Concurrency/src/main/java/Compulsory/Game.java
@@ -0,0 +1,10 @@
+package Compulsory;
+/**
+ *
+ * @author Adm
+ */
+public class Game {
+    private final Bag bag = new Bag();
+    private final Board board = new Board();
+    private final Dictionary dic
+}
