b0f58f457b3e9d6b51b54682a789d63e25dfb2c4
Bianca Crișu <100210311+crisubianca@users.noreply.github.com>
2022-04-07 17:09:15 +0300

    Create MainFrame.java

---
+++ Laborator6/PositionalGame/src/main/java/Compulsory/MainFrame.java
@@ -0,0 +1,64 @@
+package Compulsory;
+/**
+ *
+ * @author Adm
+ */
+import javax.swing.*;
+import java.awt.*;
+import static java.awt.BorderLayout.CENTER;
+
+public class MainFrame extends JFrame {
+    ConfigPanel configPanel;
+    ControlPanel controlPanel;
+    DrawingPanel canvas;
+    
+    public MainFrame(){
+        super("My drawing application");
+        init();
+    }
+    
+    private void init(){
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        
+        //create the components
+        configPanel = new ConfigPanel(this);
+        canvas = new DrawingPanel(this);
+        controlPanel = new ControlPanel(this);
+        
+        
+        //arrange the components in the container (frame)
+        //JFrame uses a BorderLayout by default
+        add(canvas, CENTER); //this is BorderLayout.CENTER
+        add(configPanel, BorderLayout.NORTH);
+        add(controlPanel, BorderLayout.SOUTH);
+        
+        //invoke the layout manager
+        pack();
+    }
+
+    public ConfigPanel getConfigPanel() {
+        return configPanel;
+    }
+
+    public void setConfigPanel(ConfigPanel configPanel) {
+        this.configPanel = configPanel;
+    }
+
+    public ControlPanel getControlPanel() {
+        return controlPanel;
+    }
+
+    public void setControlPanel(ControlPanel controlPanel) {
+        this.controlPanel = controlPanel;
+    }
+
+    public DrawingPanel getCanvas() {
+        return canvas;
+    }
+
+    public void setCanvas(DrawingPanel canvas) {
+        this.canvas = canvas;
+    }
+    
+    
+}
